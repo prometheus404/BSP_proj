a1df0758f0d796ba815b9fd17b1b88ae7912eb12
Riccardo Conforto Galli <cg.riccardo@tuta.io>
2025-02-07 12:06:25 +0100

    working permutational entropy, and test on ecg signals

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 from collections import defaultdict
 from math import log
+from wfdb.processing import ann2rr
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-x = [6, 2, 1, 4, 5, 3, 2, 1, 4, 3, 2, 1]  # timeserie
-m = 3  # embedded space size
-N = len(x)
-X = [[x[e] for e in range(i, i + m)] for i in range(N - m + 1)]
-print("timeserie: ", X)
-J = [[index for (index, _) in sorted(enumerate(X_i), key=lambda x: x[1])] for X_i in X]
-print("timeserie of indices: ", J)
+
+def embed(timeserie, m):
+    """returns a the embeddings of size m of the timeserie"""
+    return [
+        tuple([timeserie[e] for e in range(i, i + m)])
+        for i in range(len(timeserie) - m + 1)
+    ]
+
+
+def compute_toi(timeserie):
+    """computes the timeserie of indices from the input timeserie"""
+    return [
+        tuple([index for (index, _) in sorted(enumerate(t_i), key=lambda x: x[1])])
+        for t_i in timeserie
+    ]
 
 
 def probabilities(timeserie):
     """computes the probabilities of each pattern in the timeserie"""
     p = defaultdict(int)
     for t in timeserie:
-        p[tuple(t)] += 1
+        p[t] += 1
     for e in p:
-        p[tuple(e)] /= len(timeserie)
+        p[e] /= len(timeserie)
+    # print(p)
     return p
 
 
 def peEn(J):
     """computes the Shannon Entropy of the timeserie of indices"""
     p = probabilities(J)
-    return -sum([p[j_i] * log(p[j_i]) for j_i in map(tuple, J)])
+    return -sum([p[j_i] * log(p[j_i]) for j_i in p])
 
 
 # print(probabilities(J))
-print(peEn(J))
+if __name__ == "__main__":
+    x = [6, 2, 1, 4, 5, 3, 2, 1, 4, 3, 2, 1]  # timeserie
+    x = ann2rr("nsr2/nsr001", "ecg")
+    # plt.plot(x)
+    # plt.yscale("log")
+    # plt.show()
+    x2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    x2 = ann2rr("chf2/chf201", "ecg")
+    x3 = list(reversed(x2))
+    # print(x3)
+    m = 7  # embedded space size
+    X = embed(x, m)
+    J = compute_toi(X)
+    # print("timeserie of indices: ", J)
+    print("entropy:", peEn(J))
+    print("entropy of second signal: ", peEn(compute_toi(embed(x2, m))))
+    print("entropy of reversed second signal: ", peEn(compute_toi(embed(x3, m))))
+    for i in tqdm(range(1, 29)):
+        # name = f"chf2/chf2{i:02}"
+        name = f"nsr2/nsr{i:03}"
+        x = ann2rr(name, "ecg")
+        plt.plot([peEn(compute_toi(embed(x, m))) for m in tqdm(range(1, 20))])
+    # plt.plot([peEn(compute_toi(embed(x2, m))) for m in range(1, 20)])
+    plt.show()
